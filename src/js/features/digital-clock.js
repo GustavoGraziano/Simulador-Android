e18079416e7bcea1120babd8fe82e01f4c7ccedf
Gustavo Graziano <gustavograzianoa.gg@gmail.com>
2025-01-31 13:08:50 -0300

    Fiz o relógio digital funcionar

---
+++ src/js/features/digital-clock.js
@@ -0,0 +1,18 @@
+export default class DigitalClock {
+    constructor(elementId) {
+        this.element = document.getElementById(elementId)
+    }
+
+    formatTime() {
+        let date = new Date()
+
+        let hours = String(date.getHours()).padStart(2, '0')
+        let minutes = String(date.getMinutes()).padStart(2, '0')
+
+        return this.element.innerHTML = `${hours}:${minutes}`
+    }
+
+    start() {
+        setInterval(() => this.formatTime(), 100)
+    }
+}
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import DigitalClock from "./features/digital-clock.js"
+
+let digitalClock = new DigitalClock('digital-clock')
+digitalClock.start()
